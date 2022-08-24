18703d85292d3e1a8c8f805982244e4fc1a9efea
abel.kecse <abel.kecse@4p-service.de>
2022-08-24 13:33:56 +0200

    fokus changes

--- chapter03/src/test/java/chapter03/application/NativeQueryTest.java
+++ chapter03/src/test/java/chapter03/application/NativeQueryTest.java
@@ -13,7 +13,7 @@ import java.util.stream.Stream;
 
 public class NativeQueryTest {
 
-    static String db_name="CSV_DB";
+    static String db_name="FOKUS_DB";
 
 
     public static String getLatestHistoryTableName() {
